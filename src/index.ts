c2f7a5526a994fe788a8f67671fba8159b6d17ef
gift56 <efeasiughu@gmail.com>
2026-02-18 22:45:49 +0100

    fixed subject router pagination query to avoid ddos attacks

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,18 @@ import cors from "cors";
 
 const app = express();
 const PORT = 8000;
+if (!process.env.FRONTEND_URL)
+  throw new Error("FRONTEND_URL is not set in .env file");
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/api/v1/subjects", subjectsRouter);
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}))
 
 app.get("/", (req, res) => {
   res.send("Class Management API is running! 🎉🎉");
